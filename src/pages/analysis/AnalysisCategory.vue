fd843ff797113eaec49a2cfeede4420c63a4fd7f
Whatdoyumin <lym4963@naver.com>
2025-04-08 02:16:09 +0900

    chore: 가계부 개발 환경 세팅

---
+++ src/pages/analysis/AnalysisCategory.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>카테고리별 차트</div>
+</template>
+<script setup></script>
+<style scoped></style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../pages/HomeVue.vue'
+import SignupView from '@/pages/user/SignupView.vue'
+import SigninView from '@/pages/user/SigninView.vue'
+import MypageView from '@/pages/user/MypageView.vue'
+import AccountListPage from '@/pages/account/AccountListPage.vue'
+import AccountEditPage from '@/pages/account/AccountEditPage.vue'
+import AccountCreatePage from '@/pages/account/AccountCreatePage.vue'
+import AnalysisDashboard from '@/pages/analysis/AnalysisDashboard.vue'
+import AnalysisCategory from '@/pages/analysis/AnalysisCategory.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -12,34 +20,42 @@ const router = createRouter({
     {
       path: '/signup',
       name: 'signup',
+      component: SignupView,
     },
     {
       path: '/signin',
       name: 'signin',
+      component: SigninView,
     },
     {
       path: '/mypage',
       name: 'mypage',
+      component: MypageView,
     },
     {
       path: '/accounts',
       name: 'accounts',
+      component: AccountListPage,
     },
     {
       path: '/accounts/:id',
       name: 'accountDetails',
+      component: AccountEditPage,
     },
     {
       path: '/accounts/new',
       name: 'newAccount',
+      component: AccountCreatePage,
     },
     {
       path: '/dashboard',
       name: 'dashboard',
+      component: AnalysisDashboard,
     },
     {
       path: '/category',
       name: 'category',
+      component: AnalysisCategory,
     },
   ],
 })
