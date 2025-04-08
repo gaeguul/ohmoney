f4c60269c238b6c1a7578e240d38a72899d6025e
Kyung hyun <jake.roh2000@gmail.com>
2025-04-08 15:04:39 +0900

    chore: 가계부 작성 폼 ( AccountCreatePage) 개발

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 import App from './App.vue'
 import router from './router'
@@ -10,5 +11,5 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
+app.component('font-awesome-icon', FontAwesomeIcon)
 app.mount('#app')
---
+++ src/pages/account/accountAssets/AccountIcon.vue
@@ -0,0 +1,48 @@
+<template>
+  <span v-for="item in props.icons" :key="item.name">
+    <label class="submit">
+      <input type="radio" name="cash" id="cash" />
+      <i :class="item.icon" class="custom-icon-color"></i>
+      <div>{{ item.name }}</div>
+    </label>
+  </span>
+</template>
+
+<script setup>
+const props = defineProps({
+  icons: { type: Array, required: true },
+})
+</script>
+
+<style scoped>
+.submit {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.submit input[type='radio'] {
+  opacity: 0;
+}
+
+.submit input[type='radio'] + i {
+  margin-bottom: 5px;
+}
+
+.submit input[type='radio']:checked + i {
+  color: #7c4ce8;
+}
+
+.submit div {
+  width: 60px;
+  font-size: x-small;
+  text-align: center;
+}
+
+.custom-icon-color {
+  font-size: xx-large;
+  color: rgb(163, 159, 159);
+}
+span {
+  width: 60px;
+}
+</style>
