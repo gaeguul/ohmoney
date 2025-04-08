da8a1edd1e5a52a9cefe9a87d4a38b45d1dbd8a5
gaeguul <jeeham0226@gmail.com>
2025-04-08 19:29:20 +0900

    feat #5 - MonthlyAnalysisChart 컴포넌트 구현

---
+++ src/components/MonthlyAnalysisChart.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <ThreeMonthsAnalysisChart />
+    <IncomeExpenseChart />
+  </div>
+</template>
+
+<script setup>
+import IncomeExpenseChart from '@/components/IncomeExpenseChart.vue'
+import ThreeMonthsAnalysisChart from '@/components/ThreeMonthsAnalysisChart.vue'
+</script>
+
+<style></style>
