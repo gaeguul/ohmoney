8eab3e44b9e81aa132254b62eb05e4c9fc3182ca
gaeguul <jeeham0226@gmail.com>
2025-04-08 19:29:38 +0900

    feat #5 - AnalysisDashboard에 컴포넌트 추가

--- src/pages/analysis/AnalysisDashboard.vue
+++ src/pages/analysis/AnalysisDashboard.vue
@@ -1,5 +1,18 @@
 <template>
-  <div>주간/월별 차트</div>
+  <div>
+    <div>{{ currentYearMonth }}</div>
+    <MonthlySpendingChart />
+    <MonthlyAnalysisChart />
+    <WeeklyAnalysisChart />
+  </div>
 </template>
-<script setup></script>
-<style scoped></style>
+
+<script setup>
+import MonthlyAnalysisChart from '@/components/MonthlyAnalysisChart.vue'
+import MonthlySpendingChart from '@/components/MonthlySpendingChart.vue'
+import WeeklyAnalysisChart from '@/components/WeeklyAnalysisChart.vue'
+
+const currentYearMonth = '2025년 4월'
+</script>
+
+<style></style>
