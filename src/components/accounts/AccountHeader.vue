45d44be511c19423ef68372055e22f8502eae998
chaeniiiii <kce2360@gmail.com>
2025-04-09 10:38:12 +0900

    feat #3 - 페이지 설명 및 가계부 작성 버튼 개발

---
+++ src/components/accounts/AccountHeader.vue
@@ -0,0 +1,23 @@
+[AccountHeader]
+
+<template>
+  <div class="account-header px-4">
+    <h5 class="fw-bold">가계부 내역</h5>
+    <button class="btn" @click="goToAccountEditPage">가계부 작성</button>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+.account-header {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+.btn,
+.btn:active {
+  background-color: var(--color-purple-400);
+  color: white;
+}
+</style>
