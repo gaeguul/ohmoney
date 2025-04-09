d8b38611352bd6896d3dffc5d8f1048a3b316c22
Kyung hyun <jake.roh2000@gmail.com>
2025-04-09 16:59:55 +0900

    feat #9 - 도넛 그래프 추가

--- src/pages/analysis/CategoryGroup.vue
+++ src/pages/analysis/CategoryGroup.vue
@@ -3,59 +3,100 @@
     <div class="card-group">
       <div v-for="sum in summaryIncome" :key="sum.id" class="card">
         <div class="card-body">
-          <div>{{ findCategoryName(sum.categoryId) }}</div>
+          <div>{{ findCategoryName(sum) }}</div>
           <i>${{ sum.sumAmount }}</i>
         </div>
       </div>
     </div>
-    <WeeklyAnalysisChart />
+    <CategoryCircleChart
+      class="foo-chart"
+      :width="chartWidth"
+      :height="chartHeight"
+      :loading="loading"
+      :labels="summaryIncomeChart.labels"
+      :data="summaryIncomeChart.data"
+      title="카테고리 별 수입"
+    />
   </div>
 
   <div class="content">
     <div class="card-group">
       <div v-for="sum in summaryExpense" :key="sum.id" class="card">
         <div class="card-body">
-          <div>{{ findCategoryName(sum.categoryId) }}</div>
+          <div>{{ findCategoryName(sum) }}</div>
           <i>${{ sum.sumAmount }}</i>
         </div>
       </div>
     </div>
-    <WeeklyAnalysisChart />
+    <CategoryCircleChart
+      class="foo-chart"
+      :width="chartWidth"
+      :height="chartHeight"
+      :loading="loading"
+      :labels="summaryExpenseChart.labels"
+      :data="summaryExpenseChart.data"
+      title="카테고리 별 지출"
+    />
   </div>
 </template>
 <script setup>
-import WeeklyAnalysisChart from '@/components/charts/WeeklyAnalysisChart.vue'
+import CategoryCircleChart from '@/components/charts/CategoryCircleChart.vue'
+import { onMounted, reactive } from 'vue'
 
 const props = defineProps({
   summary: { type: Array, required: true },
   category: { type: Array, required: true },
 })
 
+onMounted(() => {
+  summaryIncomeChart.labels = summaryIncome.map((item) => findCategoryName(item))
+  summaryIncomeChart.data = summaryIncome.map((item) => item.sumAmount)
+
+  summaryExpenseChart.labels = summaryExpense.map((item) => findCategoryName(item))
+  summaryExpenseChart.data = summaryExpense.map((item) => item.sumAmount)
+})
+
 const summaryIncome = props.summary.filter((item) => item.categoryId > 20)
+const summaryIncomeChart = reactive({
+  labels: [],
+  data: [],
+})
 const summaryExpense = props.summary.filter((item) => item.categoryId < 20)
+
+const summaryExpenseChart = reactive({
+  labels: [],
+  data: [],
+})
+
 const findCategoryName = (sum) => {
-  console.log(props.category.expense)
+  // console.log(summaryExpense.sort((a, b) =>{ b.sumAmount - a.sumAmount}))
 
   let name = ''
-  if (sum < 20) {
-    name = props.category.expense.find((category) => category.categoryId === sum).categoryName
+  const categoryId = sum.categoryId
+
+  if (categoryId === undefined) {
+    return
+  }
+  if (categoryId < 20) {
+    name = props.category.expense.find((category) => category.categoryId === categoryId)
   } else {
-    name = props.category.income.find((category) => category.categoryId === sum).categoryName
+    name = props.category.income.find((category) => category.categoryId === categoryId)
   }
 
+  // console.log(summaryExpenseChart)
   // const name = props.category.expense.find((category) => category.categoryId === sum).categoryName
-  return name
+  return name.categoryName
 }
 </script>
 <style scoped>
 .card-body > div {
-  width: 60px;
+  width: 80px;
   font-size: 20px;
   font-size: smaller;
   font-weight: lighter;
 }
 .card-body > i {
-  width: 60px;
+  width: 80px;
   color: #7c4ce8;
   font-weight: bold;
   font-style: normal;
@@ -71,14 +112,16 @@ const findCategoryName = (sum) => {
 .card-group {
   display: flex;
   flex-wrap: wrap;
+  margin-bottom: 100px;
+  margin-top: 100px;
   gap: 8px;
 }
 
 .card {
-  width: 100px;
-  height: 70px;
+  width: 130px;
+  height: 100px;
   flex: 0 0 auto;
-  border-radius: 10px;
+  border-radius: 10%;
   background-color: white;
   margin: 4px;
 }
