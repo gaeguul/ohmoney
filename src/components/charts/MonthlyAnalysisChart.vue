dfa9292c423e9efcb2eedd71fe3613fbf704337b
gaeguul <jeeham0226@gmail.com>
2025-04-09 13:34:47 +0900

    style #5 - Background color 반응형으로 변경

---
+++ src/components/charts/MonthlyAnalysisChart.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="p-4 mt-4 monthly-analysis-container">
+    <div class="title">
+      <h4 class="fw-bold">{{ currentMonth }}월 분석</h4>
+      <div class="text-subtitle">
+        이번 달에는 지난 달보다 {{ compareToLastMonth }}원 덜 쓰셨어요!
+      </div>
+    </div>
+    <div class="analysis-container d-flex flex-col gap-4 w-100 flex-wrap">
+      <ThreeMonthsAnalysisChart />
+      <IncomeExpenseChart />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import IncomeExpenseChart from '@/components/charts/IncomeExpenseChart.vue'
+import ThreeMonthsAnalysisChart from '@/components/charts/ThreeMonthsAnalysisChart.vue'
+
+const currentMonth = 4
+const compareToLastMonth = '59,400'
+</script>
+
+<style scoped>
+.analysis-container {
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  gap: 1rem;
+}
+
+/* 641px 이상이면 가로로 배치하고, 공간을 균등 분할 */
+@media (min-width: 641px) {
+  .analysis-container {
+    flex-direction: row;
+  }
+
+  .chart-wrapper {
+    flex: 1;
+  }
+
+  .monthly-analysis-container {
+    background-color: white;
+    border-radius: 20px;
+    max-width: 500px;
+  }
+}
+</style>
