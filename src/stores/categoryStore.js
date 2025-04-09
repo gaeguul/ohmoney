080728fbd2d4a16fb1f4650006b1fc7f3138f39b
chaeniiiii <kce2360@gmail.com>
2025-04-09 16:34:07 +0900

    feat #21 - 카테고리 목록 조회 API

--- src/stores/categoryStore.js
+++ src/stores/categoryStore.js
@@ -3,13 +3,19 @@ import { ref } from 'vue'
 import axios from 'axios'
 
 export const useCategoryStore = defineStore('category', () => {
-  const categories = ref([])
+  const categories = ref({
+    income: [],
+    expense: [],
+  })
 
   const fetchCategories = async () => {
-    if (categories.value.length) return
-    const res = await axios.get('http://localhost:3000/category')
+    if (categories.value.income.length || categories.value.expense.length) return
+
+    const res = await axios.get('api/category')
     const data = res.data[0]
-    categories.value = [...data.income, ...data.expense]
+
+    categories.value.income = data.income
+    categories.value.expense = data.expense
   }
 
   return {
